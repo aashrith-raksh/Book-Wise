ba3b57a515fb61606b303a69359c5ba3943c1d58
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-03-04 12:37:05 +0530

    chore: bug fixes in email workflow

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -9,48 +9,66 @@ type InitialData = {
   fullName: string;
 };
 
-export const { POST } = serve<InitialData>(async (context) => {
-  console.log("workflow triggered")
-  const { email, fullName } = context.requestPayload;
+export const { POST } = serve<InitialData>(
+  async (context) => {
+    console.log("workflow triggered");
+    const { email, fullName } = context.requestPayload;
 
-
-  console.log("Sending welcome email to ", email)
-  await context.run("new-signup", async () => {
-    await sendEmail({
-      email,
-      subject: "Welcome to the platform",
-      message: `Welcome ${fullName}!`,
-    });
-  });
-
-  await context.sleep("wait-for-3-days", TimeInMs.THREE_DAYS);
-
-  while (true) {
-    const state = await context.run("check-user-state", async () => {
-      return await getUserState(email);
+    console.log("Sending welcome email to ", email);
+    await context.run("new-signup", async () => {
+      await sendEmail({
+        email,
+        subject: "Welcome to the platform",
+        message: `Welcome ${fullName}!`,
+      });
     });
 
-    if (state === "non-active") {
-      await context.run("send-email-non-active", async () => {
-        await sendEmail({
-          email,
-          subject: "Are you still there?",
-          message: `Hey ${fullName}, we miss you!`,
-        });
+    await context.sleep("wait-for-3-days", TimeInMs.THREE_DAYS);
+
+    while (true) {
+      const state = await context.run("check-user-state", async () => {
+        return await getUserState(email);
       });
-    } else if (state === "active") {
-      await context.run("send-email-active", async () => {
-        await sendEmail({
-          email,
-          subject: "Welcome back!",
-          message: `Welcome back ${fullName}!`,
+
+      if (state === "non-active") {
+        await context.run("send-email-non-active", async () => {
+          await sendEmail({
+            email,
+            subject: "Are you still there?",
+            message: `Hey ${fullName}, we miss you!`,
+          });
         });
-      });
+      } else if (state === "active") {
+        await context.run("send-email-active", async () => {
+          await sendEmail({
+            email,
+            subject: "Welcome back!",
+            message: `Welcome back ${fullName}!`,
+          });
+        });
+      }
+
+      await context.sleep("wait-for-1-month", TimeInMs.THIRTY_DAYS);
     }
-
-    await context.sleep("wait-for-1-month", TimeInMs.THIRTY_DAYS);
+  },
+  {
+    failureFunction: async ({
+      context,
+      failStatus,
+      failResponse,
+      failHeaders,
+    }) => {
+      console.error(`
+      ====== FAILURE FUNCTION TRIGGERED ======
+      ❌ Status: ${failStatus}
+      📩 Response: ${JSON.stringify(failResponse, null, 2)}
+      📰 Headers: ${JSON.stringify(failHeaders, null, 2)}
+      📍 Context: ${JSON.stringify(context, null, 2)}
+      =======================================
+          `);
+    },
   }
-});
+);
 
 type UserState = "non-active" | "active";
 
@@ -60,14 +78,19 @@ enum TimeInMs {
   THIRTY_DAYS = 30 * TimeInMs.ONE_DAY,
 }
 
-const getUserState = async (email: string): Promise<UserState> => {
+export const getUserState = async (email: string): Promise<UserState> => {
   const user = await db
     .select()
     .from(users)
     .where(eq(users.email, email))
     .limit(1);
 
-  if (!user) return "non-active";
+  if (!user) {
+    console.log('no user found...returning "non-active"');
+    return "non-active";
+  }
+
+  console.log(user[0]);
 
   const lastActivityDate = new Date(user[0].lastActivityStatus!);
 

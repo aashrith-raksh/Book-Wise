d50286e3615f51086f1a667de6d10a6246f6dbd3
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-03-11 17:36:17 +0530

    feat: added schema for borrow records

--- db/schema.ts
+++ db/schema.ts
@@ -47,3 +47,20 @@ export const books = pgTable("books", {
   summary: varchar("summary").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 });
+
+export const borrowRecord = pgTable("borrow_record", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  userId: uuid("user_id")
+    .references(() => users.id)
+    .notNull(),
+  bookId: uuid("book_id")
+    .references(() => books.id)
+    .notNull(),
+  borrowDate: timestamp("borrow_date", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  dueDate: date("due_date").notNull(),
+  returnDate: date("return_date"),
+  status: BORROWED_ENUM("status").default("BORROWED").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});

5f73892527d3139d64cf04e8144d830b7202e2d0
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-03-13 13:41:46 +0530

    feat: added admin redirect

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,38 @@ import { cn, getInitials } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import { Session } from "next-auth";
+import { isUserAdmin } from "@/db/utils";
 
 const Header = ({ session }: { session: Session }) => {
+  const [isAdmin, setIsAdmin] = useState<boolean | undefined>(false);
   const pathname = usePathname();
+
+  useEffect(() => {
+    async function checkAdmin() {
+      if (session?.user?.id) {
+        const result = await isUserAdmin(session.user.id);
+        setIsAdmin(result);
+      }
+    }
+    checkAdmin();
+  }, [session?.user?.id]);
   return (
     <header className="my-10 flex justify-between gap-5 items-center">
       <Link href={"/"}>
         <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
       </Link>
+
+      {isAdmin && (
+        <Link
+          className="font-semibold text-light-200 underline"
+          href={"/admin"}
+        >
+          Go to Admin Section
+        </Link>
+      )}
 
       <ul className="flex flex-row justify-between items-center gap-5">
         <li>
